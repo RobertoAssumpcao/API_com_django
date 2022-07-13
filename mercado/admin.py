8d363d60c9be0fb354f8b46ece0ac9ab56535046
Assumpção <betoassumpcao@protonmail.com>
2022-07-13 12:50:00 -0300

    banco de dados
    criação do BD.

--- mercado/admin.py
+++ mercado/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from mercado.models import Product
 
-# Register your models here.
+class Products(admin.ModelAdmin):
+    list_display = ('id','name','quantidade')
+    list_display_links = ('name', 'quantidade')
+    search_fields = ('name',)
+
